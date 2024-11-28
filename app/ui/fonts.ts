44de8ac628f738a21d6ccd234b3018be92a9bf3b
PIETKIEWICZ Andrzej Ext O-PL/Hurt <CJHP5921@pds.corpnet.pl>
2024-11-28 13:23:18 +0100

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import localFont from 'next/font/local'
+
+export const inter = localFont({ src: '../lib/fonts/Inter/static/Inter_24pt-Regular.ttf' })
+export const lusitana = localFont({ src: '../lib/fonts/Lusitana/Lusitana-Regular.ttf' })
+
